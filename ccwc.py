9ed5d38f1a1c19da483993edaefcb9642d6d5ede
Dabeer Mirza <DabeerM@hotmail.com>
2023-12-23 06:19:47 +0000

    We can now count the number of lines

--- ccwc.py
+++ ccwc.py
@@ -4,12 +4,21 @@ import os
 def filesize_bytes(file_path : str) -> int:
     return os.path.getsize(file_path)
 
+def num_of_lines(file_path : str) -> int:
+    with open(file_path) as file:
+        num_of_lines = len(file.readlines())
+    return num_of_lines
+
 parser = argparse.ArgumentParser(prog='ccwc',
                                  description='Generate various statistics about a file')
 parser.add_argument('-c', '--bytes_count', type=str)
+parser.add_argument('-l', '--lines_count', type=str)
 
 args = parser.parse_args()
 
 if args.bytes_count != None:
     file_size = filesize_bytes(args.bytes_count)
     print(str(file_size) + ' ' + args.bytes_count)
+elif args.lines_count != None:
+    line_count = num_of_lines(args.lines_count)
+    print(str(line_count) + ' ' + args.lines_count)
