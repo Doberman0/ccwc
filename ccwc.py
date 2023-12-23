af4deb15c0c1e3c19ec7df0db346bf08f2b59764
Dabeer Mirza <DabeerM@hotmail.com>
2023-12-23 06:26:24 +0000

    Add the -w command parsing logic to ccwc to return the number of words in a file

--- ccwc.py
+++ ccwc.py
@@ -9,10 +9,15 @@ def num_of_lines(file_path : str) -> int:
         num_of_lines = len(file.readlines())
     return num_of_lines
 
+# TODO: Add logic
+def num_of_words(file_path : str) -> int:
+    return 0
+
 parser = argparse.ArgumentParser(prog='ccwc',
                                  description='Generate various statistics about a file')
 parser.add_argument('-c', '--bytes_count', type=str)
 parser.add_argument('-l', '--lines_count', type=str)
+parser.add_argument('-w', '--words_count', type=str)
 
 args = parser.parse_args()
 
@@ -22,3 +27,6 @@ if args.bytes_count != None:
 elif args.lines_count != None:
     line_count = num_of_lines(args.lines_count)
     print(str(line_count) + ' ' + args.lines_count)
+elif args.words_count != None:
+    num_of_words = num_of_words(args.words_count)
+    print(str(num_of_words) + ' ' + args.words_count)
