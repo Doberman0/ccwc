413454f24f00d62346d6c2f54c6fa378df2271fa
Dabeer Mirza <DabeerM@hotmail.com>
2023-12-23 06:14:33 +0000

    We can count the number of bytes in a file now!

--- ccwc.py
+++ ccwc.py
@@ -1,4 +1,8 @@
 import argparse 
+import os
+
+def filesize_bytes(file_path : str) -> int:
+    return os.path.getsize(file_path)
 
 parser = argparse.ArgumentParser(prog='ccwc',
                                  description='Generate various statistics about a file')
@@ -6,4 +10,6 @@ parser.add_argument('-c', '--bytes_count', type=str)
 
 args = parser.parse_args()
 
-print(args.bytes_count)
+if args.bytes_count != None:
+    file_size = filesize_bytes(args.bytes_count)
+    print(str(file_size) + ' ' + args.bytes_count)
