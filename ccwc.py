91292466443059d3ec1b2625d4c53592e4f33cfe
Dabeer Mirza <DabeerM@hotmail.com>
2023-12-27 19:12:27 +0000

    Add a default "no flag" option

--- ccwc.py
+++ ccwc.py
@@ -21,11 +21,14 @@ def count_characters(file_path : str) -> int:
         char_count = len(file.read())
     newline_count = num_of_lines(file_path)
     return char_count + newline_count
+
+def nothing_flagged(args) -> bool:
+    return not (args.bytes_count or args.lines_count or args.words_count or args.characters_count)
     
 
 parser = argparse.ArgumentParser(prog='ccwc',
                                  description='Generate various statistics about a file')
-parser.add_argument('-c', '--bytes_count', action='store_true') # Change this to https://stackoverflow.com/questions/8259001/python-argparse-command-line-flags-without-arguments
+parser.add_argument('-c', '--bytes_count', action='store_true')
 parser.add_argument('-l', '--lines_count', action='store_true')
 parser.add_argument('-w', '--words_count', action='store_true')
 parser.add_argument('-m', '--characters_count', action='store_true')
@@ -33,15 +36,21 @@ parser.add_argument('filepath')
 
 args = parser.parse_args()
 
-if args.bytes_count:
+if nothing_flagged(args):
     file_size = filesize_bytes(args.filepath)
-    print(str(file_size) + ' ' + args.filepath)
-if args.lines_count:
     line_count = num_of_lines(args.filepath)
-    print(str(line_count) + ' ' + args.filepath)
-if args.words_count:
-     word_count = num_of_words(args.filepath)
-     print(str(word_count) + ' ' + args.filepath)
-if args.characters_count:
-     character_count = count_characters(args.filepath)
-     print(str(character_count) + ' ' + args.filepath)
+    word_count = num_of_words(args.filepath)
+    print(str(line_count) + ' ' + str(word_count) + ' ' + str(file_size) + ' ' + args.filepath)
+else:
+    if args.bytes_count:
+        file_size = filesize_bytes(args.filepath)
+        print(str(file_size) + ' ' + args.filepath)
+    if args.lines_count:
+        line_count = num_of_lines(args.filepath)
+        print(str(line_count) + ' ' + args.filepath)
+    if args.words_count:
+        word_count = num_of_words(args.filepath)
+        print(str(word_count) + ' ' + args.filepath)
+    if args.characters_count:
+        character_count = count_characters(args.filepath)
+        print(str(character_count) + ' ' + args.filepath)
